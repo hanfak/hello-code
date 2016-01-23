ac45772ee1ac58dd17416098fbc2755228ef101f
Hanif Fakira <fakira.work@gmail.com>
2016-01-23 09:58:58 +0000

    Update camel_case.rb

--- ruby-code/camel_case.rb
+++ ruby-code/camel_case.rb
@@ -1,15 +1,9 @@
-def to_camel_case(str, first_lower_case = false)
+def to_camel_case(str, change_first_word = false)
 
   text = str.gsub(/_|,|-/, ' ').split(" ")
-  first_word = first_lower_case ? text.shift.downcase : text.shift
+  first_word = chang_first_word ? text.shift.downcase : text.shift
   first_word + text.collect!(&:capitalize).join
 
 end
 
-#Test cases
 
-p to_camel_case("the-stealth-warrior")
-p to_camel_case("The_Stealth_Warrior")
-p to_camel_case("_The_Stealth_warrior")
-p to_camel_case("-The stealth Warrior",true)
-p to_camel_case("he_alth-barrior,BsKF", true)
