feb360864e6fddd9df663dd98f84e6cd6d6b576a
Hanif Fakira <fakira.work@gmail.com>
2016-01-24 22:56:40 +0000

    Update camel_case.rb

--- ruby-code/camel_case.rb
+++ ruby-code/camel_case.rb
@@ -1,7 +1,7 @@
 def to_camel_case(str, change_first_word = false)
 
   text = str.gsub(/_|,|-/, ' ').split(" ")
-  first_word = chang_first_word ? text.shift.downcase : text.shift
+  first_word = change_first_word ? text.shift.downcase : text.shift
   first_word + text.collect!(&:capitalize).join
 
 end
